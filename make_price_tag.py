0acd82a6a17ce4b3746bbae4399db42aba0a9549
rate1 <lislislis1986@gmail.com>
2022-02-24 01:19:14 +0800

    Add macros

--- make_price_tag.py
+++ make_price_tag.py
@@ -1,40 +1,59 @@
 import pandas as pd
+import xlrd 
+import xlwings as xw
 
-PosterID = []
-Name = []
-Code = []
-Unit = []
-Cost = []
+def write_result():
+		PosterID = []
+		Name = []
+		Code = []
+		Unit = []
+		Cost = []
 
-try:
-    data = pd.read_excel('./input.xls')
-except:
-    print("Input file must be named input.xls")
-    data = ''
+		try:
+			data = pd.read_excel('./input.xls')
+		except:
+			print("Input file must be named input.xls")
+			data = ''
 
-cols = len(data)
-print("Cols in file = ", cols)
+		cols = len(data)
+		print("Cols in file = ", cols)
 
-for x in range(cols):
-    if data.Cost[x] != data.Cost[x]: 
-        base_name = data.Name[x].strip()
-    else:
-        if data.PosterID_m[x] != data.PosterID_m[x]:
-            Name.append(data.Name[x])
-        else:
-            new_name = base_name + ' ' + data.Name[x]
-            Name.append(new_name)
+		for x in range(cols):
+			if data.Cost[x] != data.Cost[x]: 
+				base_name = data.Name[x].strip()
+			else:
+				if data.PosterID_m[x] != data.PosterID_m[x]:
+					Name.append(data.Name[x])
+				else:
+					new_name = base_name + ' ' + data.Name[x]
+					Name.append(new_name)
 
-        PosterID.append(data.PosterID[x])
-        Code.append(data.Code[x])
-        Unit.append(data.Unit[x])
-        Cost.append(data.Cost[x])
+				PosterID.append(data.PosterID[x])
+				Code.append(data.Code[x])
+				Unit.append(data.Unit[x])
+				Cost.append(data.Cost[x])
 
 
-df = pd.DataFrame({'PosterID':PosterID,
-                   'Name':Name,
-                   'Code':Code,
-                   'Unit':Unit,
-                   'Cost':Cost})
+		df = pd.DataFrame({'PosterID':PosterID,
+						   'Name':Name,
+						   'Code':Code,
+						   'Unit':Unit,
+						   'Cost':Cost})
 
-df.to_excel('./result.xls', index=False)
+		df.to_excel('./result.xls', index=False)
+
+def del_cols_rows():
+    vba_book = xw.Book("./macros.xlsm")
+    vba_macro = vba_book.macro("Get_All_File_from_Folder")
+    try:
+        vba_macro()
+    except:
+        print("Error-xyerror")
+    print("Empty rows and cols deleted")
+
+def main():
+    del_cols_rows()
+    #write_result()
+
+if __name__ == '__main__':
+    main()
